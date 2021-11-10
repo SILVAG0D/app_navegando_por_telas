b3dbf5379fda9a6197aa2729958d5f66e0aed027
SILVAG0D <liesch576@gmail.com>
2021-11-10 14:36:41 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:gs_consultoria/home_.dart';
+
+void main () {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
